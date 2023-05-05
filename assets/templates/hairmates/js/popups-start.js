84d604acdeef933c74183c7b2d7d67fba3dce58a
Dmitriy Yudichev <dimarik34rus@ya.ru>
2023-05-05 13:41:07 +0300

    update js

--- assets/templates/hairmates/js/popups-start.js
+++ assets/templates/hairmates/js/popups-start.js
@@ -4,11 +4,11 @@ const popupClose = document.querySelectorAll('.popup-start__close');
 // Закрываем попапы по нажатию на крестик
 popupClose.forEach(button => {
 	button.addEventListener('click', () => {
-		cookiePopup.classList.remove('is-active'); 
-		cookiePopup.classList.add('is-hidden'); 
-		subscribePopup.classList.remove('is-active'); 
-		subscribePopup.classList.add('is-hidden'); 
-		document.body.style.overflow = 'auto'; 
+		cookiePopup.classList.remove('is-active');
+		cookiePopup.classList.add('is-hidden');
+		subscribePopup.classList.remove('is-active');
+		subscribePopup.classList.add('is-hidden');
+		// document.body.style.overflow = 'auto'; 
 	});
 });
 
@@ -17,38 +17,25 @@ const cookiePopup = document.querySelector('.cookie-popup');
 const acceptCookie = document.querySelector('#accept-cookie');
 const cancelCookie = document.querySelector('#cancel-cookie');
 
-
-// Показываем попап при открытии страницы и убираем прокрутку у body
-if (cookiePopup.classList.contains('is-active')) { 
-	document.body.style.overflow = 'hidden'; 
-}
-
 // Если кликнули на кнопку "Принять все", то закрываем попап и включаем прокрутку у body
-acceptCookie.addEventListener('click', () => { 
-	cookiePopup.classList.remove('is-active'); 
-	cookiePopup.classList.add('is-hidden'); 
-	document.body.style.overflow = 'auto'; 
+acceptCookie.addEventListener('click', () => {
+	cookiePopup.classList.remove('is-active');
+	cookiePopup.classList.add('is-hidden');
+
 });
 
 // Если кликнули на кнопку "Отменить все", то закрываем попап и включаем прокрутку у body
-cancelCookie.addEventListener('click', () => { 
-	cookiePopup.classList.remove('is-active'); 
-	cookiePopup.classList.add('is-hidden'); 
-	document.body.style.overflow = 'auto'; 
+cancelCookie.addEventListener('click', () => {
+	cookiePopup.classList.remove('is-active');
+	cookiePopup.classList.add('is-hidden');
 });
 
 /* ------------------- SUBSCRIBE -------------------- */
 const subscribePopup = document.querySelector('.subscribe');
 const subscribeClose = document.querySelector('#subscribe-close');
 
-// Если у попапа с подпиской есть класс is-active, то отображаем его и убираем прокрутку у body 
-if (subscribePopup.classList.contains('is-active')) { 
-	document.body.style.overflow = 'hidden';
-}
-
 // Закрываем попап по нажатию на кнопку "Не сейчас"
 subscribeClose.addEventListener('click', () => {
-		subscribePopup.classList.remove('is-active'); 
-		subscribePopup.classList.add('is-hidden'); 
-		document.body.style.overflow = 'auto'; 
-	});
+	subscribePopup.classList.remove('is-active');
+	subscribePopup.classList.add('is-hidden');
+});
